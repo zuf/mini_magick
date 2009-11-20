8d6e4b3bf9ba414d3a2a3e5b1b391b8897132def
Egor Vakhromtsev <e.vakhromtsev@mediacrat.ru>
2009-11-20 15:29:53 +0300

    Replace dasherize with gsub

--- lib/mini_magick.rb
+++ lib/mini_magick.rb
@@ -169,7 +169,7 @@ module MiniMagick
     end
 
     def method_missing(symbol, *args)
-      @args << "-#{symbol.to_s.dasherize}"
+      @args << "-#{symbol.to_s.gsub('-', '_')}"
       @args += args
     end
 
